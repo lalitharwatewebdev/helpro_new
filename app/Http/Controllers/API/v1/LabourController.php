16a904e9b947e79d49abc55933b4b3b4f4c5010b
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-08-05 12:28:31 +0530

    updated

--- app/Http/Controllers/API/v1/LabourController.php
+++ app/Http/Controllers/API/v1/LabourController.php
@@ -11,10 +11,8 @@ use Illuminate\Http\Request;
 class LabourController extends Controller
 {
     public function get(){
-        $data = User::with(["states:id,name","cities:id,name"])->active()->where("labour_status","pending")
+        $data = User::with(["states:id,name","cities:id,name","labourImage:id,user_id,image"])->active()->where("labour_status","pending")
         ->where("type","labour")->get();
-
-        
 
         return response([
             "data" => $data,
