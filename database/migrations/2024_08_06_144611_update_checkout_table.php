f5ffc43d4d8545d6314ebcb9453492cdb3a16f28
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-08-06 14:49:34 +0530

    updated

---
+++ database/migrations/2024_08_06_144611_update_checkout_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('checkouts', function (Blueprint $table) {
+            $table->foreignId("address_id")->constrained()->cascadeOnDelete()->after("id");
+            $table->longText("note")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('checkouts', function (Blueprint $table) {
+            //
+        });
+    }
+};
