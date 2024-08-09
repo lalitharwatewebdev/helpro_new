52b10131f695039dc9b052cbde7ded871a357e6f
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-08-09 17:51:04 +0530

    updated

--- resources/views/content/tables/slider.blade.php
+++ resources/views/content/tables/slider.blade.php
@@ -2,78 +2,78 @@
 
 @section('title', 'Category')
 @section('page-style')
-<style>
-    [x-cloak] {
-        display: none !important;
-    }
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
 
-    .dropdown-menu {
-        transform: scale(1) !important;
-    }
-</style>
+        .dropdown-menu {
+            transform: scale(1) !important;
+        }
+    </style>
 @endsection
 
 @section('content')
 
 
-<section>
-    <div class="row match-height">
-        <div class="col-lg-12 col-md-12 col-sm-12">
-            <x-card>
-                <livewire:slider-table type="{{$type}}" />
-            </x-card>
+    <section>
+        <div class="row match-height">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <x-card>
+                    <livewire:slider-table type="{{ $type }}" />
+                </x-card>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
-<x-side-modal title="Add Slider" id="add-blade-modal">
-    <x-form id="add-slider" method="POST" class="" :route="route('admin.sliders.store')">
-        <div class="col-md-12 col-12 ">
-            <x-input name="title" />
-            <x-input-file name="image" />
-            <x-input name="link"/>
-            @php
-                $type = request()->query("type");
-            @endphp
+    <x-side-modal title="Add Slider" id="add-blade-modal">
+        <x-form id="add-slider" method="POST" class="" :route="route('admin.sliders.store')">
+            <div class="col-md-12 col-12 ">
+                <x-input name="title" />
+                <x-input-file name="image" />
+                <x-input name="link" />
+                @php
+                    $url = request()->url();
+                    $status = collect(explode('/', $url))->last();
+                @endphp
 
-            <x-input name="app_type" value="{{$type}}" type="hidden"/>
-        </div>
-    </x-form>
-</x-side-modal>
-<x-side-modal title="Update Slider" id="edit-slider-modal">
-    <x-form id="edit-slider-modal" method="POST" class="" :route="route('admin.sliders.update')">
-        <div class="col-md-12 col-12 ">
-            <x-input name="title" />
-            <x-input-file name="image" />
-            <x-input name="link"/>
-            <x-input name="id" type="hidden"/>
-        </div>
+                <x-input name="app_type" value="{{ $status }}" type="hidden" />
+            </div>
+        </x-form>
+    </x-side-modal>
+    <x-side-modal title="Update Slider" id="edit-slider-modal">
+        <x-form id="edit-slider-modal" method="POST" class="" :route="route('admin.sliders.update')">
+            <div class="col-md-12 col-12 ">
+                <x-input name="title" />
+                <x-input-file name="image" />
+                <x-input name="link" />
+                <x-input name="id" type="hidden" />
+            </div>
 
-    </x-form>
-</x-side-modal>
+        </x-form>
+    </x-side-modal>
 @endsection
 @section('page-script')
-<script>
-    $(document).ready(function() {
-        $(document).on('click', '[data-show]', function() {
-            const modal = $(this).data('show');
-            $(`#${modal}`).modal('show');
+    <script>
+        $(document).ready(function() {
+            $(document).on('click', '[data-show]', function() {
+                const modal = $(this).data('show');
+                $(`#${modal}`).modal('show');
+            });
         });
-    });
 
 
-    // $(document).on('click', function(){
-    //     $('.drop-menuToggle').removeClass('active');
-    // })
+        // $(document).on('click', function(){
+        //     $('.drop-menuToggle').removeClass('active');
+        // })
 
-    function setValue(data, modal) {
-        $(`${modal} #id`).val(data.id);
-        $(`${modal} #title`).val(data.title);
-        $(`${modal} #link`).val(data.link);
-    
-        $(modal).modal('show');
-    }
+        function setValue(data, modal) {
+            $(`${modal} #id`).val(data.id);
+            $(`${modal} #title`).val(data.title);
+            $(`${modal} #link`).val(data.link);
 
-</script>
+            $(modal).modal('show');
+        }
+    </script>
 @endsection
