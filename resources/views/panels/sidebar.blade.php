c414e05c3abe40f82ac688e96b09fe2b1cafb8e9
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-10-01 17:12:33 +0530

    web routes updated

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -27,7 +27,9 @@
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
             {{-- Foreach menu item starts --}}
             @if (isset($menuData[0]))
+               
                 @foreach ($menuData[0]->menu as $menu)
+             
                     @if (isset($menu->navheader))
                         <li class="navigation-header">
                             <span>{{ $menu->navheader }}</span>
@@ -41,6 +43,8 @@
                                 $custom_classes = $menu->classlist;
                             }
                         @endphp
+                          @if (auth("admin")->user()->role == "admin" && $menu->name =="")
+                          
                         <li
                             class="nav-item {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }} {{ $custom_classes }}">
                             <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
@@ -49,7 +53,7 @@
                                 <i class="side-bar-icon">
                                     {!! $menu->icon !!}
                                 </i>
-                                <span class="menu-title text-truncate">{{ $menu->name }}</span>
+                                <span class="menu-title text-truncate">{{ $menu->name}}</span>
                                 @if (isset($menu->badge))
                                     <?php $badgeClasses = 'badge badge-pill badge-light-primary ml-auto mr-1'; ?>
                                     <span
@@ -60,6 +64,35 @@
                                 @include('panels/submenu', ['menu' => $menu->submenu])
                             @endif
                         </li>
+                        @else
+                      
+                        @endif
+
+
+                        @if (auth("admin")->user()->role == "superadmin")
+                          
+                        <li
+                            class="nav-item {{ Route::currentRouteName() === $menu->slug ? 'active' : '' }} {{ $custom_classes }}">
+                            <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0)' }}"
+                                class="d-flex align-items-center"
+                                target="{{ isset($menu->newTab) ? '_blank' : '_self' }}">
+                                <i class="side-bar-icon">
+                                    {!! $menu->icon !!}
+                                </i>
+                                <span class="menu-title text-truncate">{{ $menu->name}}</span>
+                                @if (isset($menu->badge))
+                                    <?php $badgeClasses = 'badge badge-pill badge-light-primary ml-auto mr-1'; ?>
+                                    <span
+                                        class="{{ isset($menu->badgeClass) ? $menu->badgeClass : $badgeClasses }} ">{{ $menu->badge }}</span>
+                                @endif
+                            </a>
+                            @if (isset($menu->submenu))
+                                @include('panels/submenu', ['menu' => $menu->submenu])
+                            @endif
+                        </li>
+                        @else
+                      
+                        @endif
                     @endif
                 @endforeach
             @endif
