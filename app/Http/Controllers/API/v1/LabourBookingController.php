862553ba4867dcbeeb40a9440e1735645d0273f6
YashDhepe <yashdhepe1@gmail.com>
2025-01-07 18:31:58 +0530

    updated

--- app/Http/Controllers/API/v1/LabourBookingController.php
+++ app/Http/Controllers/API/v1/LabourBookingController.php
@@ -162,7 +162,8 @@ class LabourBookingController extends Controller
                 ->orderBy('distance')
                 ->pluck('id');
         }
-
+        \Log::info("labours_data");
+        \Log::info($labours_data);
         if (!empty($labours_data)) {
             $labours = User::whereIn('id', $labours_data)->whereHas('category', function ($query) use ($category_id) {
                 $query->where('category_id', $category_id);
@@ -313,7 +314,7 @@ class LabourBookingController extends Controller
             // $labour_payable_amount = $one_labour_amount;
 
             // $labours = LabourAcceptedBooking::where('booking_id', $labour_booking_data->id)->get();
-            foreach ($labours as $key => $value) { 
+            foreach ($labours as $key => $value) {
                 $wallet = Wallet::where('user_id', $value['labour_id'])->first();
                 $add_on_charges = ExtraTimeWork::with(['labour:id,name,email,phone'])->whereHas('labour', function ($q) use ($value) {
                     $q->where('users.id', $value['labour_id']);
